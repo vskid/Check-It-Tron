b3275f1eedda15a5dd5d93f5852ed1c9cb6107ff
rg0047 <50573628+vskid@users.noreply.github.com>
2026-04-05 01:48:02 -0700

    Update main.py

--- backend/main.py
+++ backend/main.py
@@ -19,9 +19,10 @@ app = FastAPI(
 app.add_middleware(
     CORSMiddleware,
     allow_origins=[
-        "http://localhost:5173",
-        "http://localhost:3000",
-        "http://127.0.0.1:5173",
+    "http://localhost:5173",
+    "http://localhost:3000",
+    "http://127.0.0.1:5173",
+    "https://check-it-tron.vercel.app",
     ],
     allow_credentials=True,
     allow_methods=["*"],
