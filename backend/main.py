c19e0e1e638a4c3c6d9a3c7d224ab1c78a21918a
mocharyaputra <maryaputra@ucsd.edu>
2026-04-05 02:00:03 -0700

    Update main.py

--- backend/main.py
+++ backend/main.py
@@ -22,6 +22,7 @@ app.add_middleware(
     "http://localhost:5173",
     "http://localhost:3000",
     "http://127.0.0.1:5173",
+    "http://127.0.0.1:3000",
     "https://check-it-tron.vercel.app",
     ],
     allow_credentials=True,
